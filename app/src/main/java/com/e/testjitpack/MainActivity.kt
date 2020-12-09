8a798f982518dccfe3a100ead1730bef14f755c4
Volodia Falyuta <falyuta.volodia@gmail.com>
2020-12-09 17:46:24 +0200

    - added lib b;

--- app/src/main/java/com/e/testjitpack/MainActivity.kt
+++ app/src/main/java/com/e/testjitpack/MainActivity.kt
@@ -2,10 +2,12 @@ package com.e.testjitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.e.liba.LibAMain
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LibAMain().test()
     }
 }
--- libA/src/main/java/com/e/liba/LibAMain.kt
+++ libA/src/main/java/com/e/liba/LibAMain.kt
@@ -1,9 +1,15 @@
 package com.e.liba
 
 import android.util.Log
+import com.e.libb.LibB
 
 class LibAMain {
     fun test(){
+
+        val b = LibB()
+
+        b.testLibB()
+
         Log.d("LibAMain", "test: WORKS")
     }
 }
