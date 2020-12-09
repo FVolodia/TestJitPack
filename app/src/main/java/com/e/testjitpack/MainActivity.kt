7916479c557ca090bd6f113f6a1bcd83e862fd69
Volodia Falyuta <falyuta.volodia@gmail.com>
2020-12-09 17:34:14 +0200

    - fixed deploy;

--- app/src/main/java/com/e/testjitpack/MainActivity.kt
+++ app/src/main/java/com/e/testjitpack/MainActivity.kt
@@ -2,13 +2,10 @@ package com.e.testjitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.e.liba.LibAMain
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        LibAMain().test()
     }
 }
